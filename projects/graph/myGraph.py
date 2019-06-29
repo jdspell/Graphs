f5475b02431679405d767bbc1658bb053c0e4fc7
jdspell <spelljd15@gmail.com>
2019-06-28 22:39:30 -0400

    created add vertex and add edge methods

--- projects/graph/myGraph.py
+++ projects/graph/myGraph.py
@@ -5,7 +5,8 @@ class Graph:
     def __init__(self):
         self.verticies = {}
 
-    def add_vertex():
+    def add_vertex(self, vertex):
+        self.verticies[vertex] = []
 
-
-    def add_edge():
+    def add_edge(self, vertex_one, vertex_two):
+        self.verticies[vertex_one] = self.vertices[vertex_one].append(vertex_two)
